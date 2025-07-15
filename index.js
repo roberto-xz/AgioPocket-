ca2db84337f443bc981fd438bf4e05aa85e30a0f
roberto-xz <devv-robert@outlook.com>
2025-07-15 16:17:38 -0300

    Adiciona Buffer global para compatibilidade de libs e registra Main como root component

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { registerRootComponent } from 'expo';
+import { Buffer } from 'buffer'; // import buffer
+
+// Define global.Buffer para evitar erros de libs que usam 'buffer'
+if (typeof global.Buffer === 'undefined') {
+  global.Buffer = Buffer;
+}
 
 import Main from './src/views/screens/Main';
+
 registerRootComponent(Main);
