b4b47863dadb180db7cad5f2b2d022ff08fbafdd
roberto-xz <devv-robert@outlook.com>
2025-07-17 23:13:44 -0300

    Atualiza queries SQL para refletir novo modelo de dados e tabelas

--- src/sql/queries.ts
+++ src/sql/queries.ts
@@ -11,23 +11,18 @@ const queries = {
 
     insert_user: 'INSERT INTO users (name, last, balance) VALUES (?, ?, ?);',
 
-
+    drop_client_table: 'DROP TABLE IF EXISTS clients',
     create_client_table: `
         CREATE TABLE IF NOT EXISTS clients (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(100) NOT NULL,
             last VARCHAR(100) NOT NULL,
-            cpf VARCHAR(14) UNIQUE NOT NULL,
-            birthDay VARCHAR(16) NOT NULL,
-            city VARCHAR(100),
-            neighborhood VARCHAR(100),
-            street VARCHAR(100),
-            homeNumber VARCHAR(10),
             email VARCHAR(150) UNIQUE,
-            phoneNumber VARCHAR(20)
+            phoneNumber VARCHAR(20) UNIQUE,
+            sendEmails VARCHAR(2)
         )`,
 
-    insert_client: 'INSERT INTO clients (name,last,cpf,birthDay,city,neighborhood,street,homeNumber,email,phoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    insert_client: 'INSERT INTO clients (name,last,email,phoneNumber,sendEmails) VALUES (?, ?, ?, ?, ?)',
 
     create_loan_table: `
         CREATE TABLE IF NOT EXISTS loans (
@@ -41,6 +36,17 @@ const queries = {
 
     insert_loan: 'INSERT INTO loans (client_id, value, firstInstallmentDate, percentual, installmentsCount) VALUES (?, ?, ?, ?, ?)',
 
+    create_installment_table: `
+        CREATE TABLE IF NOT EXISTS  installments (
+            id INTEGER PRIMARY KEY,
+            loanId INTEGER NOT NULL,
+            partNum INTEGER NOT NULL,
+            value REAL NOT NULL,
+            status TEXT NOT NULL,
+            dueDate TEXT NOT NULL
+        )`,
+
+    insert_installment: 'INSERT INTO Installments (loanId, partNum, value, status, dueDate) VALUES (?, ?, ?, ?, ?)',
 };
 
 export default queries;
